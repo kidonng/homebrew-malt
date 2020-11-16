dd57f4fda184dd09975c29f85d893e92c6005fbd
Kid <kidonng@gmail.com>
2020-11-16 20:08:17 +0800

    Add `ifrextract`

---
+++ Formula/ifrextract.rb
@@ -0,0 +1,11 @@
+class Ifrextract < Formula
+  desc "Extract internal forms represenation from EFI and UEFI modules"
+  homepage "https://github.com/LongSoft/Universal-IFR-Extractor"
+  url "https://github.com/LongSoft/Universal-IFR-Extractor/releases/download/v0.3.6/ifrextract_v0.3.6.osx.zip"
+  sha256 "9255e034efa54fed1c5f379d1f5ba34128a87f55794fb7a70e6f0ee562b4cafa"
+  license "GPL-3.0-only"
+
+  def install
+    bin.install "ifrextract"
+  end
+end
