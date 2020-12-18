3220ed66375636be366ddbef25c66b45e1c9f4f0
Kid <kidonng@gmail.com>
2020-12-19 00:45:20 +0800

    Update `clashx-pro` to 1.31.1.1

--- Casks/clashx-pro.rb
+++ Casks/clashx-pro.rb
@@ -1,8 +1,8 @@
 cask "clashx-pro" do
-  version "1.31.0.1"
-  sha256 "a606c0558c3f391af832ef8f1ef1c667bf0b90cad69107e9c645abb08360362f"
+  version "1.31.1.1"
+  sha256 "d5983b94c1ea0fd1a07aae8e3ba6f8af7c22ea4afa69d519748cb825356666b9"
 
-  url "https://homebrew-malt.vercel.app/api/app-center?owner=clashx&app=clashx-pro&release=37"
+  url "https://homebrew-malt.vercel.app/api/app-center?owner=clashx&app=clashx-pro&release=38"
   name "ClashX Pro"
   desc "Rule-based custom proxy with GUI based on Clash (Pro version)"
   homepage "https://github.com/yichengchen/clashX"
