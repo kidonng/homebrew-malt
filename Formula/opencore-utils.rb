c2be66512cd2789e832c6e70e35a73f81bcfefab
Kid <kidonng@gmail.com>
2021-01-06 11:40:52 +0800

    Update `opencore-utils` to 0.6.5

--- Formula/opencore-utils.rb
+++ Formula/opencore-utils.rb
@@ -1,8 +1,8 @@
 class OpencoreUtils < Formula
   desc "OpenCore utilities"
   homepage "https://github.com/acidanthera/OpenCorePkg"
-  url "https://github.com/acidanthera/OpenCorePkg/releases/download/0.6.4/OpenCore-0.6.4-RELEASE.zip"
-  sha256 "21be602e92ef218a216095c1fe89668dac0a368e96ac84129a998679bb246ee4"
+  url "https://github.com/acidanthera/OpenCorePkg/releases/download/0.6.5/OpenCore-0.6.5-RELEASE.zip"
+  sha256 "bcbc1b783ab7957973fd13ea4a5d120b0deb063120ff98a863fa7dc34c32c5c3"
   license "BSD-3-Clause"
 
   def install
