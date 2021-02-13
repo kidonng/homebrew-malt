d29b88db0f3a5c38c06058b9f99b9934c9a07b6b
Kid <kidonng@gmail.com>
2021-02-13 13:45:35 +0800

    Update `dmidecode` to 3.3b

--- Formula/dmidecode.rb
+++ Formula/dmidecode.rb
@@ -1,8 +1,8 @@
 class Dmidecode < Formula
   desc "DMI table decoder (with support for Apple/macOS)"
   homepage "https://github.com/acidanthera/dmidecode"
-  url "https://github.com/acidanthera/dmidecode/releases/download/3.3a/dmidecode-mac-3.3a.zip"
-  sha256 "d2177cd1c25e1adc058ced2a785392e0cb3622edbcb94606daeaf4c00c0667c6"
+  url "https://github.com/acidanthera/dmidecode/releases/download/3.3b/dmidecode-mac-3.3b.zip"
+  sha256 "863537f5cef9f24815fecf6ed22373547c048d1b620ec0245efaa807bd16c366"
   license "GPL-2.0-only"
 
   def install
