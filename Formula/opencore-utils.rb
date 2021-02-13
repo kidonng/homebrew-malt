12d1eff9a34daea99ee00efa852c25c2caee262b
Kid <kidonng@gmail.com>
2021-02-13 13:40:28 +0800

    Update `opencore-utils` to 0.6.6

--- Formula/opencore-utils.rb
+++ Formula/opencore-utils.rb
@@ -1,25 +1,31 @@
 class OpencoreUtils < Formula
-  desc "OpenCore utilities"
   homepage "https://github.com/acidanthera/OpenCorePkg"
-  url "https://github.com/acidanthera/OpenCorePkg/releases/download/0.6.5/OpenCore-0.6.5-RELEASE.zip"
-  sha256 "bcbc1b783ab7957973fd13ea4a5d120b0deb063120ff98a863fa7dc34c32c5c3"
+  url "https://github.com/acidanthera/OpenCorePkg/releases/download/0.6.6/OpenCore-0.6.6-RELEASE.zip"
+  sha256 "c22f34f594d5c29b991d25dfc2075de523432ffa91588c2216e4b8963c7a8f74"
   license "BSD-3-Clause"
 
   def install
     prefix.install "Utilities/CreateVault"
     bin.install_symlink "#{prefix}/CreateVault/sign.command" => "createvault"
+
     bin.install "Utilities/disklabel/disklabel" => "disklabel"
+
     bin.install "Utilities/icnspack/icnspack" => "icnspack"
+
     bin.install "Utilities/kpdescribe/kpdescribe.sh" => "kpdescribe"
+
     prefix.install "Utilities/LegacyBoot"
     bin.install_symlink "#{prefix}/LegacyBoot/BootInstall_IA32.tool" => "bootinstall_ia32"
     bin.install_symlink "#{prefix}/LegacyBoot/BootInstall_X64.tool" => "bootinstall_x64"
-    bin.install_symlink "#{prefix}/LegacyBoot/QemuBuild.command" => "qemubuild"
+
     prefix.install "Utilities/LogoutHook"
-    bin.install_symlink "#{prefix}/LogoutHook/LogoutHook.command" => "logouthook"
+    bin.install_symlink "#{prefix}/LogoutHook/LogoutHook.command" => "logouthook
+
     prefix.install "Utilities/macrecovery"
     bin.install_symlink "#{prefix}/macrecovery/macrecovery.py" => "macrecovery"
+
     bin.install "Utilities/macserial/macserial" => "macserial"
+
     bin.install "Utilities/ocvalidate/ocvalidate" => "ocvalidate"
   end
 end
