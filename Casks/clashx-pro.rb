56cfd3d7715a60d86e7c23fe10648c1072bb1e95
Kid <kidonng@gmail.com>
2021-02-20 17:30:22 +0800

    Move API out of repo

--- Casks/clashx-pro.rb
+++ Casks/clashx-pro.rb
@@ -2,7 +2,7 @@ cask "clashx-pro" do
   version "1.31.1.1"
   sha256 "d5983b94c1ea0fd1a07aae8e3ba6f8af7c22ea4afa69d519748cb825356666b9"
 
-  url "https://homebrew-malt.vercel.app/api/app-center?owner=clashx&app=clashx-pro&release=38"
+  url "https://api.xuann.wang/api/app-center?owner=clashx&app=clashx-pro&release=38"
   name "ClashX Pro"
   desc "Rule-based custom proxy with GUI based on Clash (Pro version)"
   homepage "https://github.com/yichengchen/clashX"
