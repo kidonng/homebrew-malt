fc7db30663642c8cbd16bcba6a650f2b3ac8806e
Kid <kidonng@gmail.com>
2021-03-08 23:29:58 +0800

    vercel: add 21.3.1 bottle.

--- Formula/vercel.rb
+++ Formula/vercel.rb
@@ -7,6 +7,11 @@ class Vercel < Formula
   sha256 "2b6205512e23825f181af360bf2606765d757b434120a793ab57194d2a6a18de"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/kidonng/homebrew-malt/releases/download/vercel-21.3.1"
+    sha256 cellar: :any_skip_relocation, big_sur: "59702099c1e01c899664dc184c7ceaf9d11afbf6e5f7f728a2243ef38d68e95a"
+  end
+
   depends_on "node"
 
   def install
