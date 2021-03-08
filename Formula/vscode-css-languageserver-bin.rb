06a34f978ec3c78b5fdcf37d2977ad50abfeb892
Kid <kidonng@gmail.com>
2021-03-09 07:58:49 +0800

    vscode-css-languageserver-bin: add 1.4.0 bottle.

--- Formula/vscode-css-languageserver-bin.rb
+++ Formula/vscode-css-languageserver-bin.rb
@@ -7,6 +7,11 @@ class VscodeCssLanguageserverBin < Formula
   sha256 "daa006f74a6baa265e743811bf224003048144c805bc3a9dd3f8175d4815bced"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/kidonng/homebrew-malt/releases/download/vscode-css-languageserver-bin-1.4.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "9151d3555a6b9ba8877524d0373f0806735960d3e49dbbdf78668de3524b8385"
+  end
+
   depends_on "node"
 
   def install
