3b1b4e2b01a58e7ec9582e4fd4f6c1a38e2daf1b
Kid <kidonng@gmail.com>
2021-03-09 00:28:32 +0800

    np: add 7.4.0 bottle.

--- Formula/np.rb
+++ Formula/np.rb
@@ -7,6 +7,11 @@ class Np < Formula
   sha256 "7ad8b030ae0d0a154b8ea3a50aa94cb645aa55a946cb2188408423a6a27b7506"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/kidonng/homebrew-malt/releases/download/np-7.4.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "21f804fbdc3bcb86004dfd71703d38f5947dc9d87b44509f0e61be98bcacc16a"
+  end
+
   depends_on "node"
 
   def install
