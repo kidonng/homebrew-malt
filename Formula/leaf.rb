260d13e6d51fd2af724c23df0c6ae3c6e539c517
Kid <kidonng@gmail.com>
2021-03-13 18:41:47 +0800

    Update `leaf` to 0.2.13

--- Formula/leaf.rb
+++ Formula/leaf.rb
@@ -2,9 +2,9 @@
 class Leaf < Formula
   desc "Lightweight and fast proxy utility"
   homepage "https://github.com/eycorsican/leaf"
-  version "0.2.7"
-  url "https://github.com/eycorsican/leaf/releases/download/v#{version}/leaf-x86_64-apple-darwin.zip"
-  sha256 "4403f49bad3f0ceeb10482559785f87fe561fee5458665005509b3e9d816dc99"
+  version "0.2.13"
+  url "https://github.com/eycorsican/leaf/releases/download/v#{version}/leaf-x86_64-apple-darwin.gz"
+  sha256 "6800cb367f48a584c7c0849fc9d74c97d30b6e8f3e4b2f16a25106ba2e587887"
   license "Apache-2.0"
 
   def install
