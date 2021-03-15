47505c526f2d6745a0c8d5e08eac91399c73d705
Kid <kidonng@gmail.com>
2021-03-15 16:50:58 +0800

    lsix: add 1.7.3 bottle.

--- Formula/lsix.rb
+++ Formula/lsix.rb
@@ -5,6 +5,11 @@ class Lsix < Formula
   sha256 "88f1855b0272a307758d344a861370c087432ed8f28cc95696659eb0ed7eb960"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://github.com/kidonng/homebrew-malt/releases/download/lsix-1.7.3"
+    sha256 cellar: :any_skip_relocation, big_sur: "724db452732f770ceaf25eee853d2c268da14470f2cedd4e597a99badd88bf78"
+  end
+
   depends_on "bash"
   depends_on "imagemagick"
 
