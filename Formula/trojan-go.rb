1506c0284a807620049580fbd9ae362db22dc38d
Kid <kidonng@gmail.com>
2021-04-24 11:16:07 +0800

    Update `trojan-go` to 0.8.3

--- Formula/trojan-go.rb
+++ Formula/trojan-go.rb
@@ -1,9 +1,9 @@
 class TrojanGo < Formula
   desc "Network proxy"
   homepage "https://github.com/p4gefau1t/trojan-go"
-  url "https://github.com/p4gefau1t/trojan-go/releases/download/v0.8.2/trojan-go-darwin-amd64.zip"
-  version "0.8.2"
-  sha256 "2b2490287ba97363b908075d81d622b42ddf10aac53e7bf303bbc0cbf32f6823"
+  url "https://github.com/p4gefau1t/trojan-go/releases/download/v0.8.3/trojan-go-darwin-amd64.zip"
+  version "0.8.3"
+  sha256 "67b18d4e555d37a09a91c2c40d33ef987201b68ffd9fe7ee1c2d137fd44f0e76"
   license "GPL-3.0-only"
 
   def install
