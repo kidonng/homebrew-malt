3a1a149574907b47e9677a5b8873c926dd2e80ea
Kid <kidonng@gmail.com>
2021-04-24 11:32:05 +0800

    Update `nali` to 0.2.4

--- Formula/nali.rb
+++ Formula/nali.rb
@@ -1,9 +1,9 @@
 class Nali < Formula
   desc "Tool for querying IP geographic information and CDN provider"
   homepage "https://github.com/zu1k/nali"
-  version "0.2.2"
+  version "0.2.4"
   url "https://github.com/zu1k/nali/releases/download/v#{version}/nali-darwin-amd64-v#{version}.gz"
-  sha256 "7a43e78bed7d3e21a920cda1783c40fcbcb010cf9d910dbdde9b38f0fff3b0fa"
+  sha256 "26a1ee37cc4e013b4fd929a703b1a98aa4d2a45fdb9250a3d303a9bf358a4185"
   license "MIT"
 
   def install
