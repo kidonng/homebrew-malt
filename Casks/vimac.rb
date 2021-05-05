918ea10bfb85fea522c113619098e129e05ae55d
Kid <kidonng@gmail.com>
2021-05-05 19:51:44 +0800

    Update `vimac` to 0.3.19

--- Casks/vimac.rb
+++ Casks/vimac.rb
@@ -1,8 +1,8 @@
 cask "vimac" do
-  version "0.3.18"
-  sha256 "3399746e79b4583b84f4f73c5d8f40859f92de77b41c17eba1b5c8b4bb512ed8"
+  version "0.3.19"
+  sha256 "a03ac25edca2190207c70825b154d20a3c6bc5b0d7b705d1ced95a7d0961c4a0"
 
-  url "https://api.xuann.wang/api/app-center?owner=dexterleng&app=vimac&dist=sparkle&release=30"
+  url "https://api.xuann.wang/api/app-center?owner=dexterleng&app=vimac&dist=sparkle&release=31"
   name "Vimac"
   desc "Keyboard-driven navigation and control tool"
   homepage "https://vimacapp.com/"
