c291807e09783aff2894e7c8b880fdd4a55985c5
Kid <kidonng@gmail.com>
2021-05-24 20:02:22 +0800

    pdate `trojan-go` to 0.10.0

--- Formula/trojan-go.rb
+++ Formula/trojan-go.rb
@@ -1,14 +1,14 @@
 class TrojanGo < Formula
   desc "Network proxy"
   homepage "https://github.com/p4gefau1t/trojan-go"
+  version "0.10.0"
   if Hardware::CPU.intel?
-    url "https://github.com/p4gefau1t/trojan-go/releases/download/v0.9.1/trojan-go-darwin-amd64.zip"
-    sha256 "18b383c561229b6db83b38ca82baf8652a358122388c3f96536f716839e560b9"
+    url "https://github.com/p4gefau1t/trojan-go/releases/download/v#{version}/trojan-go-darwin-amd64.zip"
+    sha256 "81d928d3bf37cd418a24ab96a6bb7c0f3f9797864e3d9729e859923642d39525"
   else
-    url "https://github.com/p4gefau1t/trojan-go/releases/download/v0.9.1/trojan-go-darwin-arm64.zip"
-    sha256 "0dc9c28e7f46a60736e67f28a96c1461e5572340066a55de980687076b518972"
+    url "https://github.com/p4gefau1t/trojan-go/releases/download/v#{version}/trojan-go-darwin-arm64.zip"
+    sha256 "d0852400728d7ec4d21ba0aab3f7703211e0f6772267040003c6a7e6d49c4bd7"
   end
-  version "0.9.1"
   license "GPL-3.0-only"
 
   def install
