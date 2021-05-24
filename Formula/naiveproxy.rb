15f6b2f0031f8fc0b68d696006a62862a37c51d5
Kid <kidonng@gmail.com>
2021-05-24 20:03:11 +0800

    Update `naiveproxy` to 90.0.4430.85-10

--- Formula/naiveproxy.rb
+++ Formula/naiveproxy.rb
@@ -1,14 +1,14 @@
 class Naiveproxy < Formula
   desc "Network proxy"
   homepage "https://github.com/klzgrad/naiveproxy"
+  version "90.0.4430.85-10"
   if Hardware::CPU.intel?
-    url "https://github.com/klzgrad/naiveproxy/releases/download/v90.0.4430.85-6/naiveproxy-v90.0.4430.85-6-mac-x64.tar.xz"
-    sha256 "01894c4e4ceb75213a73ff15e1d35ff5178127764e9ce054bb8bada2e55b4135"
+    url "https://github.com/klzgrad/naiveproxy/releases/download/v#{version}/naiveproxy-v#{version}-mac-x64.tar.xz"
+    sha256 "eb69ced497cc62c545e18ebfaced2f7172aabb2a8d81b3cfa30b897766f98f7e"
   else
-    url "https://github.com/klzgrad/naiveproxy/releases/download/v90.0.4430.85-6/naiveproxy-v90.0.4430.85-6-mac-arm64.tar.xz"
-    sha256 "5a0e59fb936bfd1451c4b1293126d115e4e4082f79ae508cb172cc227fb8cbbb"
+    url "https://github.com/klzgrad/naiveproxy/releases/download/v#{version}/naiveproxy-v#{version}-mac-arm64.tar.xz"
+    sha256 "d2fc5d857c819fc2832a0d9b866e1da92becb20cbafa0af1947316896edfc067"
   end
-  version "90.0.4430.85-6"
   license "BSD-3-Clause"
 
   def install
